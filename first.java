7d0a6bcbef845541ccd10438b8c135cd9dbcf491
suruchiprasad1321-design <suruchiprasad1321@gmail.com>
2026-06-13 14:02:52 +0530

    updated changes

--- first.java
+++ first.java
@@ -2,7 +2,8 @@
 public class first{
 
     public static void main(String[] args){
-    System.out.println("Hello student");
+    System.out.println("Hello student");\
+    System.out.println("suru");
     }
 
 }
